b7b17801197cd4bfbea2fe34311db3bc50a5bd1b
Nick Rogers <nick@nickroge.rs>
2018-04-27 00:29:26 -0400

    Update layout for players table

--- routes/players.js
+++ routes/players.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const models = require('../models');
 const sequelize = require('sequelize');
 
-router.get('/:id(\\d+)', async function(req, res, next) {
+router.get('/:id(\\d+)', async function (req, res, next) {
   try {
     let playerId = parseInt(req.params.id);
     let teamId = parseInt(req.query.team);
-    
-    let player = await models.player.findOne({ 
-      where: { id: playerId }, 
-      include: [{ 
-        model: models.team, 
-        where: { id: teamId } 
+
+    let player = await models.player.findOne({
+      where: { id: playerId },
+      include: [{
+        model: models.team,
+        where: { id: teamId }
       }]
     });
 
@@ -23,7 +23,7 @@ router.get('/:id(\\d+)', async function(req, res, next) {
 
     let playerEarnings = await player.teams[0].getPlayerEarnings(player);
 
-    res.render('players', {player: playerEarnings});
+    res.render('players', { title: `${player.name} | Earnings`, player: playerEarnings, team: player.teams[0] });
   } catch (e) {
     next(e);
   }
